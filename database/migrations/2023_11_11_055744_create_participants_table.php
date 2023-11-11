72ad202710d23579b27b3bd88c7451cc46a441f6
AlexandreGomes08 <gomesalexandre227@gmail.com>
2023-11-11 03:10:30 -0300

    creating participants migration

--- database/migrations/2023_11_11_055744_create_participants_table.php
+++ database/migrations/2023_11_11_055744_create_participants_table.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sweepstakes', function (Blueprint $table) {
-            $table->uuid("id")->primary();
-            $table->foreignId('user_id')->constrained();
-            $table->integer("number_of_winners")->default(1);
-            $table->dateTime("end_date")->nullable();
-            $table->text("description")->nullable();
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
             $table->timestamps();
         });
     }
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sweepstakes');
+        Schema::dropIfExists('participants');
     }
 };
